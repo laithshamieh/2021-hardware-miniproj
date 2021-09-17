db34afc133b4b20f6cac4b817d04fc28fb0c76de
laithshamieh <70380726+laithshamieh@users.noreply.github.com>
2021-09-17 16:50:48 -0400

    Update ble_scan.py

--- ble_scan.py
+++ ble_scan.py
@@ -13,7 +13,7 @@ p.add_argument(
     help="number of seconds to scan for BLE devices",
     nargs="?",
     type=int,
-    default=5,
+    default=60,
 )
 P = p.parse_args()
 
@@ -21,12 +21,17 @@ timeout = P.timeout
 
 print(f"Scanning BLE devices for {timeout} seconds")
 
+svc = DiscoveryService()
+ble_devs = svc.discover(timeout)
+
+# Create .txt file and write scanning statement to 
+# partition the BLE scanned data in 1 minute time slots/sessions
 f = open("hw11_miniproj_data.txt", "a")
 f.write(f"Scanning BLE devices for {timeout} seconds" + '\n')
 
 for u, n in ble_devs.items():
-    tup = (u,n)
+    tup = (u,n) # create tuple from BLEs scanned
     print(u, n)
-    f.write(''.join(tup) + '\n') 
+    f.write(''.join(tup) + '\n') # send scanned BLEs to .txt file
  
 f.close()
