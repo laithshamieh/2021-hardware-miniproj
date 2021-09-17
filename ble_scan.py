34ba4c7c33002a35baf5a1889eae42871954250a
laithshamieh <70380726+laithshamieh@users.noreply.github.com>
2021-09-17 16:59:39 -0400

    Update ble_scan.py

--- ble_scan.py
+++ ble_scan.py
@@ -4,6 +4,7 @@ Example of scanning Bluetooth Low Energy (BLE) devices.
 Requires a Linux computer due to gattlib underlying BLE scanning requiring Glib.
 """
 
+
 import argparse
 from bluetooth.ble import DiscoveryService
 
@@ -24,8 +25,11 @@ print(f"Scanning BLE devices for {timeout} seconds")
 svc = DiscoveryService()
 ble_devs = svc.discover(timeout)
 
+##
 # Create .txt file and write scanning statement to 
 # partition the BLE scanned data in 1 minute time slots/sessions
+##
+
 f = open("hw11_miniproj_data.txt", "a")
 f.write(f"Scanning BLE devices for {timeout} seconds" + '\n')
 
@@ -33,4 +37,5 @@ for u, n in ble_devs.items():
     tup = (u,n) # create tuple from BLEs scanned
     print(u, n)
     f.write(''.join(tup) + '\n') # send scanned BLEs to .txt file
+    
 f.close()
